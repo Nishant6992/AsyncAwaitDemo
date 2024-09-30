7bb5e1cb903ec0c3ebada3e290fa58036b7cb963
Nishant Raj <nishantraj6992@gmail.com>
2024-09-30 23:42:33 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AsyncAwaitDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Task1();
+            Task2();
+            Task3();
+            Task4();
+            Console.ReadLine();
+        }
+
+        public static async void Task1()
+        {
+            await Task.Run(() =>
+            {
+                Console.WriteLine("Task 1 Starting..");
+                Thread.Sleep(4000);
+                Console.WriteLine("Task 1 completed..");
+            });
+        }
+        public static async void Task2()
+        {
+            await Task.Run(() =>
+            {
+                Console.WriteLine("Task 2 Starting..");
+                Thread.Sleep(2000);
+                Console.WriteLine("Task 2 completed..");
+            });
+            Console.WriteLine("task 2 here");
+        }
+        public async static void Task3()
+        {
+            await Task.Run(() =>
+            {
+                Console.WriteLine("Task 3 Starting..");
+                Thread.Sleep(5000);
+                Console.WriteLine("Task 3 completed..");
+            });
+
+        }
+        public static async void Task4()
+        {
+            await Task.Run(() =>
+            {
+                Console.WriteLine("Task 4 Starting..");
+                Thread.Sleep(1000);
+                Console.WriteLine("Task 4 completed..");
+            });
+
+        }
+
+    }
+}
